a670c2c5321e1b5a2dda3ab7a8c6fadbae84135f
secretwolf98 <jordanluders@gmail.com>
2017-08-17 13:14:06 -0500

    Add files via upload

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,82 @@
+#
+# Copyright (C) 2012 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit LineageOS specific board config
+-include device/samsung/espressowifi/BoardConfigCM.mk
+
+# Inherit common omap4 board config
+-include hardware/ti/omap4/BoardConfigCommon.mk
+
+# Build SGX KM
+-include hardware/ti/omap4/pvr-km.mk
+
+TARGET_NO_BOOTLOADER := true
+
+TARGET_BOOTLOADER_BOARD_NAME := piranha
+
+# Inline kernel building
+TARGET_KERNEL_SOURCE := kernel/samsung/espresso10
+TARGET_KERNEL_CONFIG := espresso_defconfig
+BOARD_NAND_PAGE_SIZE := 4096
+BOARD_NAND_SPARE_SIZE := 128
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_BASE := 0x40000000
+BOARD_KERNEL_CMDLINE := androidboot.hardware=espresso
+
+# Filesystem
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 8388608
+BOARD_CACHEIMAGE_PARTITION_SIZE := 734003200
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8388608
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 5003787264
+BOARD_FLASH_BLOCK_SIZE := 4096
+
+# Disable dex-preoptimization
+WITH_DEXPREOPT := false
+
+# Configure jemalloc for low-memory
+MALLOC_SVELTE := true
+
+# Wi-Fi
+BOARD_WLAN_DEVICE                := bcmdhd
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/bcmdhd.ko"
+WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_FW_PATH_STA          := "/system/etc/wifi/bcmdhd_sta.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/system/etc/wifi/bcmdhd_apsta.bin"
+WIFI_DRIVER_FW_PATH_P2P          := "/system/etc/wifi/bcmdhd_p2p.bin"
+WIFI_DRIVER_MODULE_NAME          := "bcmdhd"
+WIFI_DRIVER_MODULE_ARG           := "firmware_path=/system/etc/wifi/bcmdhd_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt"
+WIFI_BAND                        := 802_11_ABG
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/espressowifi/bluetooth
+
+# SELinux
+BOARD_SEPOLICY_DIRS += \
+    device/samsung/espressowifi/sepolicy
+
+# Recovery
+RECOVERY_FSTAB_VERSION := 2
+TARGET_RECOVERY_FSTAB := device/samsung/espressowifi/rootdir/fstab.espresso
+TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
